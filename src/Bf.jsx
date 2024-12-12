9760930aa46f3ab2821a2b6143948d43612670a0
sreeventh <sreeventhsrinivasan@gmail.com>
2024-12-12 13:47:06 +0530

    compressed_for_deployed_version

--- src/Bf.jsx
+++ src/Bf.jsx
@@ -14,7 +14,8 @@ import {
 import { Plane, Html } from "@react-three/drei";
 import DeskModel from "../public/Desk.jsx";
 import { TextureLoader, RepeatWrapping } from "three";
-import MModel from "../public/M85.jsx";
+// import MModel from "../public/M85.jsx";
+import M85Comp from "../public/M85Comp.jsx";
 import { useLoader, useFrame } from "@react-three/fiber";
 
 function World() {
@@ -76,7 +77,7 @@ function World() {
         <DeskModel position={[8, 0, -3]} scale={0.6} />
       </Suspense>
       <Suspense>
-        <MModel position={[0, 0, 1]} scale={0.3} exploded={exploded} />
+        <M85Comp position={[0, 0, 1]} scale={0.3} exploded={exploded} />
       </Suspense>
       <Button
         onClick={toggleExplode}
