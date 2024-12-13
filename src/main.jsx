32dd0bd68959bc575b32bdeb0a33f3ca1b84563b
sreeventh <sreeventhsrinivasan@gmail.com>
2024-12-13 22:28:08 +0530

    sf

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import World from "../public/StarterFinal.jsx";
+import World from "../public/Starter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
